4d2e0151593f4c36fc08f8e48f6c91c5e3e0e50a
Bhavesh Yadav <codezera3@gmail.com>
2023-07-01 17:01:26 +0530

    backend folder structure setup done

--- server/index.js
+++ server/index.js
@@ -2,5 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import postRoutes from './routes/posts.js'
 
 const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://CodeZera3:hWamgfFLyAgHDdkB@mern-social-media-app.itnpmx6.mongodb.net/';
+const PORT = process.env.PORT || 6000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, ()=>{console.log(`Server running on port: ${PORT}`)}))
+    .catch((err) => {console.log(err.message)})
+
+
+// hWamgfFLyAgHDdkB
